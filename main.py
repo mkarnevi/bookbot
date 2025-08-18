1fc5fa5e8a87438ae3638d75b83ea7e59f19036e
Mike Karnevi <mkarnevi@users.noreply.github.com>
2025-08-18 12:55:40 +0200

    completed CH2L6

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import book_word_count
+from stats import book_word_count, book_character_count
 
 def get_book_text(filepath):
   with open(filepath) as f:
@@ -10,5 +10,7 @@ def main():
   contents = get_book_text(path)
   word_count = book_word_count(contents)
   print(f"{word_count} words found in the document")
-
+  
+  dictionary = book_character_count(contents)
+  print(dictionary)
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,8 @@
 def book_word_count(contents):
   return len(contents.split())
+
+def book_character_count(contents):
+  characters_dict = {}
+  for character in contents.lower():
+    characters_dict[character] = characters_dict.get(character, 0) + 1
+  return characters_dict
