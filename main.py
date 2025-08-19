92bcccc912c88efefed24eb741a5b2dd8fdadf5e
Mike Karnevi <mkarnevi@users.noreply.github.com>
2025-08-19 10:37:38 +0200

    completed CH2L7

--- main.py
+++ main.py
@@ -5,12 +5,31 @@ def get_book_text(filepath):
     file_contents = f.read()
     return file_contents
 
+def sort_on(items):
+    return items["number"]
+
+def convert_and_sort_dictionary(dictionary):
+  unsorted_dictionary_list = [{"char": character, "number": number} for character, number in dictionary.items()]
+
+  unsorted_dictionary_list.sort(reverse=True, key=sort_on)
+  return unsorted_dictionary_list
+
 def main():
   path = "books/frankenstein.txt"
   contents = get_book_text(path)
   word_count = book_word_count(contents)
-  print(f"{word_count} words found in the document")
   
   dictionary = book_character_count(contents)
-  print(dictionary)
+  sorted_dictionary = convert_and_sort_dictionary(dictionary)
+
+  print("============ BOOKBOT ============")
+  print(f"Analyzing book found at {path}...")
+  print("----------- Word Count ----------")
+  print(f"Found {word_count} total words")
+  print("--------- Character Count -------")
+  for entry in sorted_dictionary:
+    if entry["char"].isalpha():
+      print(f"{entry['char']}: {entry['number']}")
+  print("============= END ===============")
+
 main()
